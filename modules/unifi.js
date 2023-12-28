0f78ab8b8ff1363eaaccaa606a5eb43c2837edc7
Sandro Lange <lange@vi-bim.de>
2023-12-28 22:32:25 +0100

    fix: resolves some layout issues

--- modules/unifi.js
+++ modules/unifi.js
@@ -90,8 +90,8 @@ module.exports.getExistingVouchers = () => {
                             duration: time(voucher.duration),
                             type: voucher.status === 'VALID_MULTI' ? 'multi-use' : 'single-use',
                             usage_quota: voucher.quota === 1 ? `${voucher.qos_usage_quota} MB`: 'unlimited',
-                            upload_limit: voucher.qos_rate_max_up !== "undefined" ? `${voucher.qos_rate_max_up} KBit/s`: 'unlimited',
-                            download_limit: voucher.qos_rate_max_down !== "undefined" ? `${voucher.qos_rate_max_down} KBit/s` : 'unlimited'
+                            upload_limit: voucher.qos_rate_max_up !== undefined ? `${voucher.qos_rate_max_up} KBit/s`: 'unlimited',
+                            download_limit: voucher.qos_rate_max_down !== undefined ? `${voucher.qos_rate_max_down} KBit/s` : 'unlimited'
                         });
                     });
                     resolve(vouchers);
