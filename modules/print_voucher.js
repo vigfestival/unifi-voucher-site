46e845e142dd1a114b0fc81e79a5a0e4fb4a5bec
Sandro Lange <lange@vi-bim.de>
2023-12-29 21:45:03 +0100

    chore: trying to support label printer to print vouchers

--- modules/print_voucher.js
+++ modules/print_voucher.js
@@ -13,18 +13,21 @@ const config = {
 module.exports.printVoucher = async (voucher) => {
     let printer = new ThermalPrinter({
         type: config.printer.type,                                  // Printer type: 'star', 'epson', 'brother' etc.
-        interface: config.printer.useTcp ? `tcp://${config.printer.ip}` : config.printer.interface,  // Printer interface
-        removeSpecialCharacters: false,                           // Removes special characters - default: false
-        lineCharacter: "=",                                       // Set character for lines - default: "-"
-        breakLine: BreakLine.WORD,                                // Break line after WORD or CHARACTERS. Disabled with NONE - default: WORD
-        options: {                                                 // Additional options
-            timeout: 5000                                           // Connection timeout (ms) [applicable only for network printers] - default: 3000
-        }
+        interface: config.printer.useTcp ? `tcp://${config.printer.ip}` : config.printer.interface  // Printer interface
     });
     let isConnected = await printer.isPrinterConnected();       // Check if printer is connected, return bool of status
 
     let printSuccessful = true;
     try {
+        printer.getStatus();
+
+        try {
+            const status = await printer.execute({ waitForResponse: true });
+            console.log('Printer status:', status);
+        } catch (e) {
+            console.error('Print failed:', e);
+        }
+
         printer.alignCenter();
         printer.newLine();
         printer.println("WiFi Voucher Code");// Append text with new line
