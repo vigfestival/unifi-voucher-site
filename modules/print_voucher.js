d58f55623befbf1c918a9757b0bc26303b909805
Sandro Lange <lange@vi-bim.de>
2023-12-30 18:42:32 +0100

    fix: various bugfixes

--- modules/print_voucher.js
+++ modules/print_voucher.js
@@ -43,7 +43,7 @@ async function createVoucherImage(voucher) {
     context.fillStyle = '#ffffff'
     context.fillRect(0, 0, width, height)
 
-    const paddingTop = 15;
+    const paddingTop = 30;
 
     context.textAlign = 'center'
     context.textBaseline = 'middle';
@@ -55,13 +55,13 @@ async function createVoucherImage(voucher) {
     let upload_limit = voucher.qos_rate_max_up !== undefined ? `${voucher.qos_rate_max_up} KBit/s` : 'unlimited';
     let download_limit = voucher.qos_rate_max_down !== undefined ? `${voucher.qos_rate_max_down} KBit/s` : 'unlimited';
 
-    context.font = 'bold 30pt Arial';
-    context.fillText('WiFi Voucher Code', midWidth, paddingTop);
     context.font = 'bold 35pt Arial';
-    context.fillText(`${[voucher.code.slice(0, 5), '-', voucher.code.slice(5)].join('')}`, midWidth, paddingTop + 35 + 20);
-    context.font = '25pt Arial';
-    context.fillText(`Duration: ${duration} | Type: ${type}`, midWidth, paddingTop + 35 + 20 + 40 + 10);
-    context.fillText(`Quota: ${usage_quota} | Download: ${download_limit} | Upload: ${upload_limit}`, midWidth, paddingTop + 35 + 20 + 40 + 10 + 30);
+    context.fillText('WiFi Voucher Code', midWidth, paddingTop);
+    context.font = 'bold 40pt Arial';
+    context.fillText(`${[voucher.code.slice(0, 5), '-', voucher.code.slice(5)].join('')}`, midWidth, paddingTop + 40 + 25);
+    context.font = '27pt Arial';
+    context.fillText(`Duration: ${duration} | Type: ${type}`, midWidth, paddingTop + 40 + 25 + 45 + 15);
+    context.fillText(`Quota: ${usage_quota} | Download: ${download_limit} | Upload: ${upload_limit}`, midWidth, paddingTop + 40 + 25 + 45 + 15 + 32);
 
     const buffer = canvas.toBuffer('image/png');
     fs.writeFileSync(`${voucher._id}.png`, buffer);
